0fbad2112c1212501d73d094e9047aa75f410a84
Andreasgdp <39928082+Andreasgdp@users.noreply.github.com>
2019-01-11 15:05:06 +0100

    Added some spaces to 6. step, so it is more readable

--- Biblioteker_og_docstrings_rumgeometri/6. step.py
+++ Biblioteker_og_docstrings_rumgeometri/6. step.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Point():
     '''
     Repræsenterer et punkt i rummet
@@ -17,7 +18,8 @@ class Point():
 class Vector():
     '''
     Repræsenterer en vector i rummet
-    '''    
+    '''
+
     def __init__(self, x, y, z):
         '''
         Returnerer en vector
@@ -32,41 +34,40 @@ class Vector():
         Retunerer en stedvektor til et punkt.
         '''
         return cls(p.x, p.y, p.z)
-    
-    @classmethod    
+
+    @classmethod
     def forbindende_vektor(cls, x1, y1, z1, x2, y2, z2):
         '''
         Retunerer en forbindende vektor.                                                                
         '''
         return cls(x2 - x1, y2 - y1, z2 - z1)
-    
-    @classmethod # statisk metode som gør at man kan skrive vector.classens navn (vector, da classen hedder "vector")
+
+    # statisk metode som gør at man kan skrive vector.classens navn (vector, da classen hedder "vector")
+    @classmethod
     def sumvektor(cls, v1, v2):
         '''
         Tager summen af to vektorer.
         '''
         return cls(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z)
-    
+
     @classmethod
     def differens(cls, v1, v2):
         '''
         Tager differensen af to vektorer.
         '''
         return cls(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z)
-    
-    
+
     def length(self):
         '''
         Retunerer længden af vektoren, hvor man har en vektor som input. Altså ikke længden mellem to punkter.
         '''
         return math.sqrt(self.x**2 + self.y**2 + self.z**2)
-    
 
     def __str__(self):
         '''
         Laver formatet vektorer udskrives på.
         '''
-        return "({}, {}, {})".format(self.x, self.y, self.z)        
+        return "({}, {}, {})".format(self.x, self.y, self.z)
 
 
 p1 = Point(1, 2, 3)
