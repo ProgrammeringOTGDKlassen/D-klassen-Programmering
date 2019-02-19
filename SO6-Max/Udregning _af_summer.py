b5609b6e7de4abdde4fe61caab136b008fab9740
Maxx0758 <32611523+Maxx0758@users.noreply.github.com>
2019-02-19 14:35:50 +0100

    Stor update
    Har fået tilføjet sammenligning med andre summer, som virker. Tilføjet det til alle sum funktionerne. Testet at det virker som det skal.

--- SO6-Max/Udregning _af_summer.py
+++ SO6-Max/Udregning _af_summer.py
@@ -59,15 +59,6 @@ def trapez_sum(xs, ys):
         s2 = (s + s1)/2
     return s2
 
-def bedre_trapez_sum(f, a, b, n):
-    s = 0
-    x = linspace(a, b, n)
-    for i in range(0, n - 1):
-        dx = x[i + 1] - x[i]
-        h = 0.5 * (f(x[i + 1]) + f(x[i]))
-        s += dx * h
-    return s
-
 '''
 print(s)
 xs = linspace(a,b,n)
@@ -92,13 +83,34 @@ while True:
             n = int(list1[2])
             xs = linspace(a, b, n)
             ys = f_liste(f, xs)
-            Result = venstre_sum(xs, ys)
+            ys2 = f_midliste(f,xs)
+            Venstre_result = venstre_sum(xs, ys)
             integral = quad(f, a, b)
             integral_værdi = integral[0]
             integral_fejl = integral[1]
-            sammenligning = Result - integral_værdi
-            print('Venstre summen af {} i intervallet {} til {} med {} inddelinger er {}. Det er {} fra det eksakte integral som er {} med en fejl på {}'.format(Funktion, a, b, n, Result, sammenligning, integral_værdi, integral_fejl))
-        
+            Venstre_sammenligning = Venstre_result - integral_værdi
+            Højre_result = højre_sum(xs, ys)
+            Højre_sammenligning = Højre_result - integral_værdi
+            Mid_result = midt_sum(xs,ys2)
+            Mid_sammenligning = Mid_result - integral_værdi
+            Trapez_result = trapez_sum(xs, ys)
+            Trapez_sammenligning = Trapez_result - integral_værdi
+            Sammenlignings_liste = [Venstre_sammenligning, Højre_sammenligning, Mid_sammenligning, Trapez_sammenligning]
+            Absolut_liste = [abs(Venstre_sammenligning), abs(Højre_sammenligning), abs(Mid_sammenligning), abs(Trapez_sammenligning)]
+            Mindsteværdi = min(Absolut_liste)
+            for i in range(0,len(Absolut_liste)):
+                if Mindsteværdi == Absolut_liste[i]:
+                    Listeelement = Sammenlignings_liste[i]
+            if Listeelement == Venstre_sammenligning:
+                Bedste_sum = "Venstresum"
+            elif Listeelement == Højre_sammenligning:
+                Bedste_sum = "Højresum"
+            elif Listeelement == Mid_sammenligning:
+                Bedste_sum = "Midsum"
+            elif Listeelement == Trapez_sammenligning:
+                Bedste_sum = "Trapezum"
+            print('Venstre summen af {} i intervallet {} til {} med {} inddelinger er {}. Det er {} fra det eksakte integral som er {} med en fejl på {}'.format(Funktion, a, b, n, Venstre_result, Venstre_sammenligning, integral_værdi, integral_fejl))
+            print('Den bedste sum er {} som er {} fra det eksakte integral som er {}.'.format(Bedste_sum, Listeelement, integral_værdi))
         elif msg == "midsum":
             Funktion = input("Indtast funktionen du vil have en sum af [Der skal være * mellem et tal og x som fx 3x -> 3*x. Opløftninger skal gøres med **]: ")
             Inddeling = input("Indtast start, slut og antal inddelinger i formatet [a b n]: ")
@@ -108,13 +120,35 @@ while True:
             n = int(list1[2])
             xs = linspace(a, b, n)
             ys = f_midliste(f, xs)
-            Result = midt_sum(xs, ys)
+            ys2 = f_liste(f,xs)
+            Mid_result = midt_sum(xs, ys)
             integral = quad(f, a, b)
             integral_værdi = integral[0]
             integral_fejl = integral[1]
-            sammenligning = Result - integral_værdi
-            print('Mid summen af {} i intervallet {} til {} med {} inddelinger er {}. Det er {} fra det eksakte integral som er {} med en fejl på {}'.format(Funktion, a, b, n, Result, sammenligning, integral_værdi, integral_fejl))
-        
+            Mid_sammenligning = Mid_result - integral_værdi
+            Højre_result = højre_sum(xs, ys2)
+            Højre_sammenligning = Højre_result - integral_værdi
+            Venstre_result = venstre_sum(xs,ys2)
+            Venstre_sammenligning = Venstre_result - integral_værdi
+            Trapez_result = trapez_sum(xs,ys2)
+            Trapez_sammenligning = Trapez_result - integral_værdi
+            Sammenlignings_liste = [Venstre_sammenligning, Højre_sammenligning, Mid_sammenligning, Trapez_sammenligning]
+            Absolut_liste = [abs(Venstre_sammenligning), abs(Højre_sammenligning), abs(Mid_sammenligning), abs(Trapez_sammenligning)]
+            Mindsteværdi = min(Absolut_liste)
+            for i in range(0,len(Absolut_liste)):
+                if Mindsteværdi == Absolut_liste[i]:
+                    Listeelement = Sammenlignings_liste[i]
+            if Listeelement == Venstre_sammenligning:
+                Bedste_sum = "Venstresum"
+            elif Listeelement == Højre_sammenligning:
+                Bedste_sum = "Højresum"
+            elif Listeelement == Mid_sammenligning:
+                Bedste_sum = "Midsum"
+            elif Listeelement == Trapez_sammenligning:
+                Bedste_sum = "Trapezum"
+            print('Mid summen af {} i intervallet {} til {} med {} inddelinger er {}. Det er {} fra det eksakte integral som er {} med en fejl på {}'.format(Funktion, a, b, n, Mid_result, Mid_sammenligning, integral_værdi, integral_fejl))
+            print('Den bedste sum er {} som er {} fra det eksakte integral som er {}.'.format(Bedste_sum, Listeelement, integral_værdi))
+
         elif msg == "højresum":
             Funktion = input("Indtast funktionen du vil have en sum af [Der skal være * mellem et tal og x som fx 3x -> 3*x. Opløftninger skal gøres med **]: ")
             Inddeling = input("Indtast start, slut og antal inddelinger i formatet [a b n]: ")
@@ -124,12 +158,34 @@ while True:
             n = int(list1[2])
             xs = linspace(a, b, n)
             ys = f_liste(f, xs)
-            Result = højre_sum(xs, ys)
+            ys2 = f_midliste(f, xs)
+            Højre_result = højre_sum(xs, ys)
             integral = quad(f, a, b)
             integral_værdi = integral[0]
             integral_fejl = integral[1]
-            sammenligning = Result - integral_værdi
-            print('Højre summen af {} i intervallet {} til {} med {} inddelinger er {}. Det er {} fra det eksakte integral som er {} med en fejl på {}'.format(Funktion, a, b, n, Result, sammenligning, integral_værdi, integral_fejl))
+            Højre_sammenligning = Højre_result - integral_værdi
+            Venstre_result = venstre_sum(xs, ys)
+            Venstre_sammenligning = Venstre_result - integral_værdi
+            Mid_result = midt_sum(xs,ys2)
+            Mid_sammenligning = Mid_result - integral_værdi
+            Trapez_result = trapez_sum(xs,ys)
+            Trapez_sammenligning = Trapez_result - integral_værdi
+            Sammenlignings_liste = [Venstre_sammenligning, Højre_sammenligning, Mid_sammenligning, Trapez_sammenligning]
+            Absolut_liste = [abs(Venstre_sammenligning), abs(Højre_sammenligning), abs(Mid_sammenligning), abs(Trapez_sammenligning)]
+            Mindsteværdi = min(Absolut_liste)
+            for i in range(0,len(Absolut_liste)):
+                if Mindsteværdi == Absolut_liste[i]:
+                    Listeelement = Sammenlignings_liste[i]
+            if Listeelement == Venstre_sammenligning:
+                Bedste_sum = "Venstresum"
+            elif Listeelement == Højre_sammenligning:
+                Bedste_sum = "Højresum"
+            elif Listeelement == Mid_sammenligning:
+                Bedste_sum = "Midsum"
+            elif Listeelement == Trapez_sammenligning:
+                Bedste_sum = "Trapezum"
+            print('Højre summen af {} i intervallet {} til {} med {} inddelinger er {}. Det er {} fra det eksakte integral som er {} med en fejl på {}'.format(Funktion, a, b, n, Højre_result, Højre_sammenligning, integral_værdi, integral_fejl))
+            print('Den bedste sum er {} som er {} fra det eksakte integral som er {}.'.format(Bedste_sum, Listeelement, integral_værdi))
 
         elif msg == "trapezsum":
             Funktion = input("Indtast funktionen du vil have en sum af [Der skal være * mellem et tal og x som fx 3x -> 3*x. Opløftninger skal gøres med **]: ")
@@ -138,24 +194,40 @@ while True:
             a = int(list1[0])
             b = int(list1[1])
             n = int(list1[2])
-            diff = 1
-            last = 0
             xs = linspace(a, b, n)
-            while diff > 0.0001:
-                s = bedre_trapez_sum(f, a, b, n)
-                diff = abs(last - s)
-                last = s
-                n *= 2
+            ys = f_liste(f, xs)
+            ys2 = f_midliste(f, xs)
+            Trapez_result = trapez_sum(xs,ys)
             integral = quad(f, a, b)
             integral_værdi = integral[0]
             integral_fejl = integral[1]
-            sammenligning = s - integral_værdi
-            print('Trapez summen af {} i intervallet {} til {} med {} inddelinger er {}. Det er {} fra det eksakte integral som er {} med en fejl på {}'.format(Funktion, a, b, n, s, sammenligning, integral_værdi, integral_fejl))
+            Trapez_sammenligning = Trapez_result - integral_værdi
+            Højre_result = højre_sum(xs, ys)
+            Højre_sammenligning = Højre_result - integral_værdi
+            Mid_result = midt_sum(xs,ys2)
+            Mid_sammenligning = Mid_result - integral_værdi
+            Venstre_result = venstre_sum(xs, ys)
+            Venstre_sammenligning = Venstre_result - integral_værdi
+            Sammenlignings_liste = [Venstre_sammenligning, Højre_sammenligning, Mid_sammenligning, Trapez_sammenligning]
+            Absolut_liste = [abs(Venstre_sammenligning), abs(Højre_sammenligning), abs(Mid_sammenligning), abs(Trapez_sammenligning)]
+            Mindsteværdi = min(Absolut_liste)
+            for i in range(0,len(Absolut_liste)):
+                if Mindsteværdi == Absolut_liste[i]:
+                    Listeelement = Sammenlignings_liste[i]
+            if Listeelement == Venstre_sammenligning:
+                Bedste_sum = "Venstresum"
+            elif Listeelement == Højre_sammenligning:
+                Bedste_sum = "Højresum"
+            elif Listeelement == Mid_sammenligning:
+                Bedste_sum = "Midsum"
+            elif Listeelement == Trapez_sammenligning:
+                Bedste_sum = "Trapezum"
+            print('Trapez summen af {} i intervallet {} til {} med {} inddelinger er {}. Det er {} fra det eksakte integral som er {} med en fejl på {}'.format(Funktion, a, b, n, Trapez_result, Trapez_sammenligning, integral_værdi, integral_fejl))
+            print('Den bedste sum er {} som er {} fra det eksakte integral som er {}.'.format(Bedste_sum, Listeelement, integral_værdi))
 
         if msg == ("quit"): #Hvis msg == quit så stopper programmet
             break
     except Exception as e:
         print(e)
         print("[ERROR] Except was runned! Check your code!!!")
-        
 print("Tak for nu")
