812511440313ff871dbe25a2067aa97d0b233402
Andreasgdp <39928082+Andreasgdp@users.noreply.github.com>
2019-05-18 14:18:47 +0200

    Formaterede kattefod (layout)

--- Kattefødder (mat) - Bens, Frederik T & Formand/kattefod.py
+++ Kattefødder (mat) - Bens, Frederik T & Formand/kattefod.py
@@ -3,7 +3,6 @@ from math import sqrt
 from math import pi
 import cv2
 
-
 img = cv2.imread('new pictures/Vinkler/Vinkler cropped/DSC_0067.JPG')
 
 hvideKoordinater = []
@@ -29,16 +28,16 @@ for y, line in enumerate(img):
 def afstand1(pixels):
     return 631.72*(1/(pixels)**0.46956)
 
-# Den funktion vi går med!
-def afstand(pixels):
+
+def afstand(pixels):  # Den funktion vi går med!
     return 536.22*(1/(pixels)**0.47936)
 
 
 def afstand3(pixels):
     return 476.63*(1/(pixels)**0.46544)
 
-# Den metode vi går med!
-def areal_af_cirkel(hvideKoordinater):
+
+def areal_af_cirkel(hvideKoordinater):  # Den metode vi går med!
     førsteHvideKoordinat = hvideKoordinater[0]
     sidsteHvideKoordinat = hvideKoordinater[-1]
     cirkelHøjde = sidsteHvideKoordinat[1] - førsteHvideKoordinat[1]
