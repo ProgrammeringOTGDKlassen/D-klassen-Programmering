3f8025acf51a917810cd681e36861d2c522f09be
Andreas <Andreasgdp@gmail.com>
2019-09-11 09:56:49 +0200

    Added loading video playing

---
+++ Arbejde med databaser - Andreas P/DAMP/test.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture('./Loading screen/Loading screen.mp4')
+
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  success,frame = cap.read()
+
+  if success == True:
+
+    # Display the resulting frame
+    cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    cv2.imshow("window", frame)
+    # cv2.imshow('Frame',frame)
+
+    # Press Q on keyboard to exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+
+  # Break the loop
+  else: 
+    break
+
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
