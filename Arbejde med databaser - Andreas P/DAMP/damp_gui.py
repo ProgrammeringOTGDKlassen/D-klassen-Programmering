45abbcb20593594779441c87add10ce4697bb61c
Andreas <Andreasgdp@gmail.com>
2019-09-11 12:39:01 +0200

    added comments and a relative path to icon

--- Arbejde med databaser - Andreas P/DAMP/damp_gui.py
+++ Arbejde med databaser - Andreas P/DAMP/damp_gui.py
@@ -19,15 +19,19 @@ class Damp_Gui(ttk.Frame):
     def build_GUI(self):
         pass
 
+
 root = tk.Tk()
 # sets the icon top left to e predefined icon
-root.iconbitmap(r'C:\Users\andre\OneDrive - Syddansk Erhvervsskole\GitHub\D-klassen-Programmering\Arbejde med databaser - Andreas P\DAMP\Icons\DAMP_ICON.ico')
+root.iconbitmap(r'.\Icons\DAMP_ICON.ico')
 # defines the default windows size
 root.geometry("1280x720")
 # starts windows in maximized size
 root.state('zoomed')
 
-app = Damp_Gui(root)
-app.master.title('DAMP')
 
+# defines the app inheriting root as base
+app = Damp_Gui(root)
+# sets the title of the app
+app.master.title('DAMP')
+# starts the app
 app.mainloop()
