1baa1a00465771339a45f0bcd645d5f8bbd84b8b
Andreas <andreasgdp@gmail.com>
2019-11-17 10:45:12 +0100

    Formatted code and added init function where to add the image for special function

--- Tile_matching - Andreas P/spilprogrammering/game.py
+++ Tile_matching - Andreas P/spilprogrammering/game.py
@@ -1,44 +1,48 @@
 import random
 
-class Tile():
-    def __init__(self):
-        self.color = random.randint(1,5)
-        self.special = random.randint(1,1) if random.randint(1, 100) == 1 else None
 
-class Game():
+class Tile:
     def __init__(self):
-        self.grid = [[Tile() for y in range(0,10)] for x in range(0,10)]
-        self.anim = [[0 for y in range(0,10)] for x in range(0,10)]
+        self.color = random.randint(1, 5)
+        self.special = random.randint(1, 1) if random.randint(1, 100) == 1 else None
+
+
+class Game:
+    def __init__(self):
+        self.grid = [[Tile() for y in range(0, 10)] for x in range(0, 10)]
+        self.anim = [[0 for y in range(0, 10)] for x in range(0, 10)]
         self.user_press = False
         self.points = 0
         self.detect_matches()
 
     def build_grid(self):
-        #import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         for x in range(0, len(self.grid)):
             for y in range(0, len(self.grid)):
                 if self.grid[x][y].color == 0:
-                    if y < len(self.grid[x])-1 and not all(self.grid[x][yy].color == 0 for yy in range(y, len(self.grid[x]))):
+                    if y < len(self.grid[x]) - 1 and not all(
+                        self.grid[x][yy].color == 0
+                        for yy in range(y, len(self.grid[x]))
+                    ):
                         # Flyt kolonnen ned
-                        while(self.grid[x][y].color == 0):
+                        while self.grid[x][y].color == 0:
                             self.grid[x][y:] = self.shift_column(self.grid[x][y:], 1)
                             if self.user_press:
-                                self.anim[x][y:] = [50 for i in range(y,len(self.anim[x]))]
+                                self.anim[x][y:] = [
+                                    50 for i in range(y, len(self.anim[x]))
+                                ]
                     # Fyld op med nye tiles
-                    for fill in range(0,len(self.grid[x])):
+                    for fill in range(0, len(self.grid[x])):
                         if self.grid[x][fill].color == 0:
-                            self.grid[x][fill].color = random.randint(1,5)
-
+                            self.grid[x][fill].color = random.randint(1, 5)
 
     def shift_column(self, l, n):
         return l[n:] + l[:n]
 
-
-    def swap_tiles(self, x1,y1,x2,y2):
-        #Sørg for, at vi kun kan bytte naboceller.
-        if abs(x1-x2) <= 1 and abs(y1-y2) <= 1:
+    def swap_tiles(self, x1, y1, x2, y2):
+        # Sørg for, at vi kun kan bytte naboceller.
+        if abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1:
             self.grid[x1][y1], self.grid[x2][y2] = self.grid[x2][y2], self.grid[x1][y1]
-
 
     def detect_sweeper(self, match_list):
         special = False
@@ -48,59 +52,72 @@ class Game():
 
         return special
 
-
     def detect_matches(self):
-        for x in range(1, len(self.grid)-1):
+        for x in range(1, len(self.grid) - 1):
             for y in range(0, len(self.grid)):
-                #Detect horizontal match
-                if self.grid[x][y].color == self.grid[x-1][y].color and self.grid[x][y].color == self.grid[x+1][y].color:
+                # Detect horizontal match
+                if (
+                    self.grid[x][y].color == self.grid[x - 1][y].color
+                    and self.grid[x][y].color == self.grid[x + 1][y].color
+                ):
                     c = self.grid[x][y].color
-                    match_list = [self.grid[x-1][y], self.grid[x][y], self.grid[x+1][y]]
-                    x1 = x+2
+                    match_list = [
+                        self.grid[x - 1][y],
+                        self.grid[x][y],
+                        self.grid[x + 1][y],
+                    ]
+                    x1 = x + 2
                     while x1 < len(self.grid) and self.grid[x1][y].color == c:
                         match_list.append(self.grid[x1][y])
                         x1 += 1
 
                     if not self.detect_sweeper(match_list):
-                        self.grid[x-1][y].color = 0
+                        self.grid[x - 1][y].color = 0
                         self.grid[x][y].color = 0
-                        self.grid[x+1][y].color = 0
-                        x1 = x+2
+                        self.grid[x + 1][y].color = 0
+                        x1 = x + 2
                         while x1 < len(self.grid) and self.grid[x1][y].color == c:
                             self.grid[x1][y].color = 0
                             x1 += 1
                         self.points += 10
                     else:
-                        for i in range(0,10):
+                        for i in range(0, 10):
                             self.grid[i][y].color = 0
                             self.points += 10
 
-                    #Hvis vi har fjernet brikker, skal pladen fyldes igen
+                    # Hvis vi har fjernet brikker, skal pladen fyldes igen
                     self.build_grid()
-        for y in range(1, len(self.grid)-1):
+        for y in range(1, len(self.grid) - 1):
             for x in range(0, len(self.grid)):
-                #Detect vertical match
-                if self.grid[x][y].color == self.grid[x][y-1].color and self.grid[x][y].color == self.grid[x][y+1].color:
+                # Detect vertical match
+                if (
+                    self.grid[x][y].color == self.grid[x][y - 1].color
+                    and self.grid[x][y].color == self.grid[x][y + 1].color
+                ):
                     c = self.grid[x][y].color
-                    match_list = [self.grid[x][y-1], self.grid[x][y], self.grid[x][y+1]]
-                    y1 = y+2
+                    match_list = [
+                        self.grid[x][y - 1],
+                        self.grid[x][y],
+                        self.grid[x][y + 1],
+                    ]
+                    y1 = y + 2
                     while y1 < len(self.grid) and self.grid[x][y1].color == c:
                         match_list.append(self.grid[x][y1])
                         y1 += 1
                     if not self.detect_sweeper(match_list):
-                        self.grid[x][y-1].color = 0
+                        self.grid[x][y - 1].color = 0
                         self.grid[x][y].color = 0
-                        self.grid[x][y+1].color = 0
-                        y1 = y+2
+                        self.grid[x][y + 1].color = 0
+                        y1 = y + 2
                         while y1 < len(self.grid) and self.grid[x][y1].color == c:
                             self.grid[x][y1].color = 0
                             y1 += 1
                         self.points += 10
                     else:
-                        for i in range(0,10):
+                        for i in range(0, 10):
                             self.grid[x][i].color = 0
                             self.points += 10
-                    #Hvis vi har fjernet brikker, skal pladen fyldes igen
+                    # Hvis vi har fjernet brikker, skal pladen fyldes igen
                     self.build_grid()
         if not self.user_press:
             self.points = 0
--- Tile_matching - Andreas P/spilprogrammering/tile_matching.py
+++ Tile_matching - Andreas P/spilprogrammering/tile_matching.py
@@ -3,19 +3,26 @@ from game import Game
 
 # Setup pygame
 pygame.init()
-screen = pygame.display.set_mode((800, 600))#, pygame.FULLSCREEN)
+screen = pygame.display.set_mode((800, 600))  # , pygame.FULLSCREEN)
 myfont = pygame.font.SysFont("monospace", 12)
 clock = pygame.time.Clock()
 
 # Initialize game variables
 done = False
 game = Game()
-current_tile = (3,3)
+current_tile = (3, 3)
 
 # tile vars
-tile_colors = [(0,0,0), (255,0,0), (0,255,0), (0,0,255), (255,255,0), (0,255,255)]
-tile_offset = [280,530]
-tile_size = [50,50]
+tile_colors = [
+    (0, 0, 0),
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (255, 255, 0),
+    (0, 255, 255),
+]
+tile_offset = [280, 530]
+tile_size = [50, 50]
 
 restart_dim = (85, 30)
 restart_pos = (120, 100)
@@ -30,66 +37,144 @@ pygame.mixer.music.load("./Fruit Level - Candy Crush Saga.ogg")
 
 
 def draw_game():
-    pygame.draw.rect(screen, (0,0,0), pygame.Rect(0,0,800,600))
+    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, 800, 600))
     if current_tile is not None:
         t = abs((pygame.time.get_ticks() % 512) - 256) % 256
-        c = (t,t,t)
-        pygame.draw.rect(screen, c, pygame.Rect(tile_offset[0] + current_tile[0]*tile_size[0] - 3, tile_offset[1] - (current_tile[1]+1)*tile_size[1] - 3, tile_size[0], tile_size[1]))
-    for y in range(0,len(game.grid)):
-        for x in range(0,len(game.grid[y])):
+        c = (t, t, t)
+        pygame.draw.rect(
+            screen,
+            c,
+            pygame.Rect(
+                tile_offset[0] + current_tile[0] * tile_size[0] - 3,
+                tile_offset[1] - (current_tile[1] + 1) * tile_size[1] - 3,
+                tile_size[0],
+                tile_size[1],
+            ),
+        )
+    for y in range(0, len(game.grid)):
+        for x in range(0, len(game.grid[y])):
             if game.anim[x][y] > 0:
                 game.anim[x][y] -= 1
                 if game.anim[x][y] == 0:
                     game.detect_matches()
-            # TODO: add image to show the soecial blocks
+            
             sc_r, sc_g, sc_b = tile_colors[game.grid[x][y].color]
             scale_color = 0.5
             sc_r = sc_r * scale_color
             sc_g = sc_g * scale_color
             sc_b = sc_b * scale_color
             special_color = (sc_r, sc_g, sc_b)
-            
-            pygame.draw.rect(screen, tile_colors[game.grid[x][y].color] if game.grid[x][y].special == None else special_color, pygame.Rect(tile_offset[0] + x*tile_size[0], tile_offset[1] - (y+1)*tile_size[1] - game.anim[x][y], tile_size[0]-5, tile_size[1]-5))
+
+            pygame.draw.rect(
+                screen,
+                tile_colors[game.grid[x][y].color]
+                if game.grid[x][y].special == None
+                else special_color,
+                pygame.Rect(
+                    tile_offset[0] + x * tile_size[0],
+                    tile_offset[1] - (y + 1) * tile_size[1] - game.anim[x][y],
+                    tile_size[0] - 5,
+                    tile_size[1] - 5,
+                ),
+            )
+            draw_special(x, y, game.grid[x][y].special)
             if not game.user_press:
                 game.detect_matches()
-    screen.blit(myfont.render("Du har {} point".format(game.points), 0, (255,255,255)), (50, 50))
-    
+    screen.blit(
+        myfont.render("Du har {} point".format(game.points), 0, (255, 255, 255)),
+        (50, 50),
+    )
+
     pos = pygame.mouse.get_pos()
 
-    if restart_pos[0] < pos[0] < restart_pos[0] + restart_dim[0] and restart_pos[1] < pos[1] < restart_pos[1] + restart_dim[1]:
-        pygame.draw.rect(screen, (255, 220, 115), pygame.Rect(restart_pos[0], restart_pos[1], restart_dim[0], restart_dim[1]))
+    if (
+        restart_pos[0] < pos[0] < restart_pos[0] + restart_dim[0]
+        and restart_pos[1] < pos[1] < restart_pos[1] + restart_dim[1]
+    ):
+        pygame.draw.rect(
+            screen,
+            (255, 220, 115),
+            pygame.Rect(restart_pos[0], restart_pos[1], restart_dim[0], restart_dim[1]),
+        )
     else:
-        pygame.draw.rect(screen, (255, 207, 64), pygame.Rect(restart_pos[0], restart_pos[1], restart_dim[0], restart_dim[1]))
-    screen.blit(myfont.render("RESTART", 0, (0, 0, 0)), (restart_txt[0], restart_txt[1]))
+        pygame.draw.rect(
+            screen,
+            (255, 207, 64),
+            pygame.Rect(restart_pos[0], restart_pos[1], restart_dim[0], restart_dim[1]),
+        )
+    screen.blit(
+        myfont.render("RESTART", 0, (0, 0, 0)), (restart_txt[0], restart_txt[1])
+    )
 
-    if reset_score_pos[0] < pos[0] < reset_score_pos[0] + reset_score_dim[0] and reset_score_pos[1] < pos[1] < reset_score_pos[1] + reset_score_dim[1]:
-        pygame.draw.rect(screen, (255, 220, 115), pygame.Rect(reset_score_pos[0], reset_score_pos[1], reset_score_dim[0], reset_score_dim[1]))
+    if (
+        reset_score_pos[0] < pos[0] < reset_score_pos[0] + reset_score_dim[0]
+        and reset_score_pos[1] < pos[1] < reset_score_pos[1] + reset_score_dim[1]
+    ):
+        pygame.draw.rect(
+            screen,
+            (255, 220, 115),
+            pygame.Rect(
+                reset_score_pos[0],
+                reset_score_pos[1],
+                reset_score_dim[0],
+                reset_score_dim[1],
+            ),
+        )
     else:
-        pygame.draw.rect(screen, (255, 207, 64), pygame.Rect(reset_score_pos[0], reset_score_pos[1], reset_score_dim[0], reset_score_dim[1]))
-    screen.blit(myfont.render("Reset score", 0, (0, 0, 0)), (reset_score_txt[0], reset_score_txt[1]))
+        pygame.draw.rect(
+            screen,
+            (255, 207, 64),
+            pygame.Rect(
+                reset_score_pos[0],
+                reset_score_pos[1],
+                reset_score_dim[0],
+                reset_score_dim[1],
+            ),
+        )
+    screen.blit(
+        myfont.render("Reset score", 0, (0, 0, 0)),
+        (reset_score_txt[0], reset_score_txt[1]),
+    )
+
+# TODO: add image to show the soecial blocks
+def draw_special(x, y, special):
+    if special == 1:
+        pass
+    else:
+        pass
 
 
-def pixels_to_cell(x,y):
-    x1 = int((x - tile_offset[0])/tile_size[0])
-    y1 = int((-y + tile_offset[1])/tile_size[1])
-    return x1,y1
+def pixels_to_cell(x, y):
+    x1 = int((x - tile_offset[0]) / tile_size[0])
+    y1 = int((-y + tile_offset[1]) / tile_size[1])
+    return x1, y1
 
 
 pygame.mixer.music.play(-1)
-#Main game loop
+# Main game loop
 while not done:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+        if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+        ):
             done = True
 
-        #Håndtering af input fra mus
+        # Håndtering af input fra mus
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            x_cell, y_cell = pixels_to_cell(pos[0],pos[1])
-            if restart_pos[0] < pos[0] < restart_pos[0] + restart_dim[0] and restart_pos[1] < pos[1] < restart_pos[1] + restart_dim[1]:
+            x_cell, y_cell = pixels_to_cell(pos[0], pos[1])
+            if (
+                restart_pos[0] < pos[0] < restart_pos[0] + restart_dim[0]
+                and restart_pos[1] < pos[1] < restart_pos[1] + restart_dim[1]
+            ):
                 game = Game()
-            
-            if reset_score_pos[0] < pos[0] < reset_score_pos[0] + reset_score_dim[0] and reset_score_pos[1] < pos[1] < reset_score_pos[1] + reset_score_dim[1]:
+
+            if (
+                reset_score_pos[0] < pos[0] < reset_score_pos[0] + reset_score_dim[0]
+                and reset_score_pos[1]
+                < pos[1]
+                < reset_score_pos[1] + reset_score_dim[1]
+            ):
                 game.points = 0
 
             if 0 <= x_cell < len(game.grid) and 0 <= y_cell < len(game.grid[0]):
@@ -99,11 +184,11 @@ while not done:
                 else:
                     game.swap_tiles(x_cell, y_cell, current_tile[0], current_tile[1])
                     current_tile = None
-                    #Når der er byttet brikker, kan vi kontrollere om der er lavet et match
+                    # Når der er byttet brikker, kan vi kontrollere om der er lavet et match
                     game.detect_matches()
 
     draw_game()
 
-    #pygame kommandoer til at vise grafikken og opdatere 60 gange i sekundet.
+    # pygame kommandoer til at vise grafikken og opdatere 60 gange i sekundet.
     pygame.display.flip()
     clock.tick(60)
