b2b9195ad75024effc1e47da5cac08febcc0e80d
Max <maxx0758@gmail.com>
2020-01-07 15:17:52 +0100

    Start on changing speeds on the snake

--- Snake - Max/snake.py
+++ Snake - Max/snake.py
@@ -56,12 +56,14 @@ class cube(object):
 class snake(object):
     body = []
     turns = {}
+    
     def __init__(self, color, pos):
         self.color = color
         self.head = cube(pos, 'images\snakeHead.png')
         self.body.append(self.head)
         self.dirnx = 0
         self.dirny = 1
+        self.time_to_move = 10
  
     def move(self):
         for event in pygame.event.get():
@@ -90,20 +92,23 @@ class snake(object):
                     self.dirnx = 0
                     self.dirny = 1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
-        for i, c in enumerate(self.body):
-            p = c.pos[:]
-            if p in self.turns:
-                turn = self.turns[p]
-                c.move(turn[0],turn[1])
-                if i == len(self.body)-1:
-                    self.turns.pop(p)
-            else:
-                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
-                else: c.move(c.dirnx,c.dirny)
+
+        self.time_to_move -= 1
+        if self.time_to_move <= 0:
+            self.time_to_move = 10
+            for i, c in enumerate(self.body):
+                p = c.pos[:]
+                if p in self.turns:
+                    turn = self.turns[p]
+                    c.move(turn[0],turn[1])
+                    if i == len(self.body)-1:
+                        self.turns.pop(p)
+                else:
+                    if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                    elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                    elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                    elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                    else: c.move(c.dirnx,c.dirny)
        
  
     def reset(self, pos):
@@ -223,8 +228,7 @@ def main():
     clock = pygame.time.Clock()
     ti = 10
     while flag:
-        pygame.time.delay(50)
-        clock.tick(10)
+        clock.tick(100)
         s.move()
         if s.body[0].pos == snack.pos:
             s.addCube()
