bd71386e1520aba6f44fb2265ad4aa144f1d375e
Andreas <andreasgdp@gmail.com>
2020-03-18 09:44:46 +0100

    Almost working first assignment and beginning of second
    Co-authored-by: jaco145a@edu.sde.dk <jaco145a@edu.sde.dk>
    Co-authored-by: Max Hansen <maxx0758@gmail.com>

--- matematiktræning - Andreas P & co/math_training.py
+++ matematiktræning - Andreas P & co/math_training.py
@@ -5,8 +5,8 @@ from flask import render_template
 from flask import session
 from flask import redirect
 from flask import url_for
-from random import randint
-from math import tan, radians, dist
+from random import randint, uniform
+from math import tan, radians, dist, sqrt, pi
 
 app = Flask(__name__)
 app.secret_key = "very secret string"
@@ -40,10 +40,13 @@ def opg():
     if opg_id == 1:
         return generate_skaering_to_linjer()
     if opg_id == 2:
+        pass
+    if opg_id == 3:
         return generate_two_circle_intersect()
+    if opg_id == 4:
+        return generate_circular_arc()
     else:
-        # Tilføj selv flere opgaver...
-        return my_render("skaeringtolinjer.html", title="Skæring mellem to linjer")
+        print("Stor gay")
 
 
 @app.route("/submit_answer", methods=["POST"])
@@ -91,13 +94,13 @@ def generate_skaering_to_linjer():
 
 
 def get_circle_radius(cent1, cent2, distance):
-
-    r1 = randint(1, distance)
-    r2 = randint(1, (r1 + distance))
+    r1 = uniform(1, distance)
+    r2 = uniform(1, (r1 + distance))
     rdist = r1 + r2
     # * checks
-    if not rdist >= distance:
-        raise ValueError("Radius too short")
+    while not rdist >= distance:
+        r2 = uniform(1, (r1 + distance))
+        rdist = r1 + r2
     if r1 > r2 + distance or r2 > r1 + distance:
         raise ValueError("Radius too long")
     if not r1 >= 1 or not r1 >= 1:
@@ -105,11 +108,12 @@ def get_circle_radius(cent1, cent2, distance):
     return (r1, r2)
 
 
-def get_intercetions(x0, y0, r0, x1, y1, r1):
+def get_intercetions(x0, y0, r0, x1, y1, r1, d):
     # circle 1: (x0, y0), radius r0
     # circle 2: (x1, y1), radius r1
 
-    d = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+    # The afstandsformel
+    # d = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
 
     # non intersecting
     if d > r0 + r1:
@@ -120,9 +124,20 @@ def get_intercetions(x0, y0, r0, x1, y1, r1):
     # coincident circles
     if d == 0 and r0 == r1:
         return None
+    if d == r0 + r1:
+        # vector1 = (x0, y0)
+        # vector2 = (x1, y1)
+        x3, y3 = (((x1 - x0) / 2) + x0, ((y1 - y0) / 2) + y0)
+        x4, y4 = None, None
+    if (d + r1) == r0:
+        x3, y3 = (((x1 - x0) / d) * r0, (((x1 - x0)) / d) * r0)
+        x4, y4 = None, None
+    if (d + r0) == r1:
+        x3, y3 = (((x0 - x1) / d) * r1, (((x0 - x1)) / d) * r1)
+        x4, y4 = None, None
     else:
         a = (r0 ** 2 - r1 ** 2 + d ** 2) / (2 * d)
-        h = math.sqrt(r0 ** 2 - a ** 2)
+        h = sqrt(r0 ** 2 - a ** 2)
         x2 = x0 + a * (x1 - x0) / d
         y2 = y0 + a * (y1 - y0) / d
         x3 = x2 + h * (y1 - y0) / d
@@ -145,22 +160,50 @@ def generate_two_circle_intersect():
 
     c1r, c2r = get_circle_radius(cent1, cent2, distance)
 
-    x1, y1, x2, y3 = get_intercetions(c1x, c1y, c1r, c2x, c2y, c2r)
+    x1, y1, x2, y2 = get_intercetions(c1x, c1y, c1r, c2x, c2y, c2r, distance)
+
+    two_points = True
+    if x2 == None or y2 == None:
+        two_points = False
 
     if two_points:
-        qid = register_answer("({},{}), ({},{})".format(x1, y1, x2, y3))
+        x1, y1, x2, y2 = (round(x1, 2), round(y1, 2), round(x2, 2), round(y2, 2))
+        qid = register_answer("({},{}), ({},{})".format(x1, y1, x2, y2))
     else:
+        x1, y1 = (round(x1, 2), round(y1, 2))
         qid = register_answer("({},{})".format(x1, y1))
 
     return my_render(
         "intersecttwocircle.html",
         title="Skæring mellem to linjer",
         opg_id=qid,
-        a1=a1,
-        a2=a2,
-        b1=b1,
-        b2=b2,
+        c1x=c1x,
+        c1y=c1y,
+        c2x=c2x,
+        c2y=c2y,
+        c1r=c1r,
+        c2r=c2r,
+        x1=x1,
+        x2=x2,
+        y1=y1,
+        y2=y2,
     )
+
+
+def generate_circular_arc():
+    length = randint(1, 100)
+    qid = register_answer("{}".format(length))
+    angle = randint(1, 180)
+    r = length/((v/180)*pi)
+    return my_render(
+        "circulararc.html",
+        title="Skæring mellem to linjer",
+        opg_id=qid,
+        r=r,
+        angle=angle,
+    )
+
+
 
 
 if __name__ == "__main__":
