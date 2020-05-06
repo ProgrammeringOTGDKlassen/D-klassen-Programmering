8dbc71e525f48b6b5ce27291101b9101a468d003
maxx0758 <maxx0758@gmail.com>
2020-05-06 14:08:48 +0200

    Removed unused function

--- Eksamensprojekt - Max/econodata.py
+++ Eksamensprojekt - Max/econodata.py
@@ -4,16 +4,12 @@ from datetime import datetime as dt
 from collections import OrderedDict
 
 class User():
-    
     def __init__(self, first_name: str, last_name: str, username: str, email: str, password: str):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.password = password
         self.email = email
-
-    def set_id(self, id):
-        self.id = id
 
 
 class EconomyData():
